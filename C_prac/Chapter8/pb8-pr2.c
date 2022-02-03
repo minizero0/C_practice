6523c272256ed19b9022473a13eaacd89f3d571d
yeongmin <dudals1069@gmail.com>
2022-02-03 15:21:00 +0900

    02-03

---
+++ C_prac/Chapter8/pb8-pr2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	num;
+	printf("start\n");
+	for (num = 1; num < 20; num++)
+	{
+		if (num % 2 == 0 || num % 3 == 0)
+			continue;
+		printf("%d\n", num);
+	}
+	printf("end\n");
+}
