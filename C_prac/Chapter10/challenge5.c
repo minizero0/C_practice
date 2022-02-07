a2b80b9e6936e56455671f9530d8ac7258f7bd9a
yeongmin <dudals1069@gmail.com>
2022-02-07 14:23:55 +0900

    02-07

---
+++ C_prac/Chapter10/challenge5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//소수  출력
+int	main(void)
+{
+	printf("1\n");
+	int j;
+	for (int i = 1; i < 100; i++)
+	{
+		for (j = 2; j <= i; j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (i == j)
+			printf("%d\n", i);
+	}
+}
