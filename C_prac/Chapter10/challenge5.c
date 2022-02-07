ce59bd01cd1d4591e8cc81df6575ea5a1cf80907
yeongmin <dudals1069@gmail.com>
2022-02-07 21:55:36 +0900

    02-07

--- C_prac/Chapter10/challenge5.c
+++ C_prac/Chapter10/challenge5.c
@@ -2,15 +2,14 @@
 //소수  출력
 int	main(void)
 {
-	printf("1\n");
+	printf ("1\n");
+
 	int j;
-	for (int i = 1; i < 100; i++)
+	for (int i = 1; i < 200; i++)
 	{
 		for (j = 2; j <= i; j++)
-		{
 			if (i % j == 0)
 				break;
-		}
 		if (i == j)
 			printf("%d\n", i);
 	}
--- C_prac/Chapter10/challenge8.c
+++ C_prac/Chapter10/challenge8.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 //재귀를 이용해 2의 n제곱 구하기
 
-int	Recur(int num)
+int	Recur(int n)
 {
-	if (num == 0)
+	if (n == 0)
 		return 1;
-	return Recur(num - 1) * 2;
+	return (Recur(n - 1) * 2);
+
 }
 
-int	main(void)
+int main(void)
 {
-	int num ;
+	int	n;
 	printf("정수를 입력하세요\n");
-	scanf("%d", &num);
-	printf("2의 %d승은 = %d\n",num, Recur(num));
+	scanf("%d", &n);
+	printf("2의 %d 제곱은 = %d\n",n, Recur(n));
 }
