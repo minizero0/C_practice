4d0aca46c476393a164bd65d4182ec117f9700f1
yeongmin <dudals1069@gmail.com>
2022-02-07 21:48:42 +0900

    02-07

--- C_prac/Chapter10/challenge4.c
+++ C_prac/Chapter10/challenge4.c
@@ -8,11 +8,11 @@ int	main(void)
 	int coke = 400;
 	int cr, sh, co;
 
-	printf("현재 당신이 소유하고 있는 금액 : %d원\n", num);
+	printf("당신이 소유하고 있는 금액 : %d원\n", num);
 	for (cr = 1; cr < num / cream; cr++)
 		for (sh = 1; sh < num / shrimp; sh++)
 			for (co = 1; co < num / coke; co++)
-				if (num == cream * cr + shrimp * sh + coke * co)
+				if (num == cr * cream + sh * shrimp + co * coke)
 					printf("크림빵 %d개, 새우깡 %d개, 콜라 %d개\n", cr, sh, co);
 	printf("어떻게 구입하시겠습니까?\n");
 }
