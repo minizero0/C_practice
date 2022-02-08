f2ace9fee68687991c4269ca46267a283a29215c
yeongmin <dudals1069@gmail.com>
2022-02-08 21:28:23 +0900

    02-08

--- C_prac/Chapter12/pb1-1.c
+++ C_prac/Chapter12/pb1-1.c
@@ -4,12 +4,14 @@ int	main(void)
 {
 	int num1 = 10;
 	int num2 = 20;
+	int	tmp;
 	int *ptr1 = &num1;
 	int *ptr2 = &num2;
 	*ptr1 += 10;
 	*ptr2 -= 20;
-	*ptr1 = num2;
-	*ptr2 = num1;
+	tmp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = tmp;
 	printf("ptr1 = %d\n", *ptr1);
 	printf("ptr2 = %d\n", *ptr2);
 }
