ce72cadc2c20ef80b5aa15b95578f3f0389df5b7
yeongmin <dudals1069@gmail.com>
2022-02-12 15:37:16 +0900

    02-012

--- C_prac/Chapter13/pb1-3.c
+++ C_prac/Chapter13/pb1-3.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-int ft_strlen(int *n)
-{
-	int index = 0;
-	while (*n)
-		index++;
-	return (index);
-}
-
 int	main(void)
 {
 	int arr[] = {1, 2, 3, 4, 5, 6};
-	int index = 0;
-	index = ft_strlen(arr);
-	printf("%d\n", index);
-
+	int index = 5;
+	for (int i = 0; i < index / 2; i++)
+	{
+		int tmp;
+		tmp = arr[i];
+		arr[i] = arr[index - i];
+		arr[index - i] = tmp;
+	}
+	for (int i = 0; i < 6; i++)
+		printf("%d ", arr[i]);
 }
