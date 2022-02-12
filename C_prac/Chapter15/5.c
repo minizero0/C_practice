0499ba532f3f2eb8f0cf6000408a44e7a023c681
yeongmin <dudals1069@gmail.com>
2022-02-12 21:34:49 +0900

    02-012

--- C_prac/Chapter15/5.c
+++ C_prac/Chapter15/5.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 
 
+
 int	main(void)
 {
 	int arr[7];
 	int len = sizeof(arr) / sizeof(int);
-	printf("%d\n", len);
-
+	int i, j;
+	
+	for (int i = 0; i < len; i++)
+	{
+		int num;
+		printf("입력 : ");
+		scanf("%d", &num);
+		arr[i] = num;
+	}
+	i = 0;
+	while (i < len - 1)
+	{
+		j = 0;
+		while (j < len - 1)
+		{
+			if (arr[j] > arr[i])
+			{
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	while (len)
+	{
+		printf("%d", arr[len - 1]);
+		len--;
+	}
 }
