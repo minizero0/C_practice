e9994df0e881bd997a2261f06af99b9f2b8413b4
yeongmin <dudals1069@gmail.com>
2022-05-20 13:15:53 +0900

    BJ

--- C_prac/Backjune/배열/2577.c
+++ C_prac/Backjune/배열/2577.c
@@ -2,19 +2,15 @@
 
 int	main(void)
 {
-	int x,y,z;
-	int arr[100] = { 0, };
-
+	int x, y, z, sum;
+	int arr[10] = {};
 	scanf("%d %d %d", &x, &y, &z);
 
-	int sum = x * y * z;
-
+	sum = x * y * z;
 	while (sum > 0){
-		arr[sum%10]++;
+		arr[sum % 10]++;
 		sum /= 10;
 	}
-	
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < 10; i++)
 		printf("%d\n", arr[i]);
-	}
 }
