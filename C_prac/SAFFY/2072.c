b1cd1e3747feda570f46b87cd804ef668763baeb
yeongmin <dudals1069@gmail.com>
2022-05-22 14:05:12 +0900

    BJ

--- C_prac/SAFFY/2072.c
+++ C_prac/SAFFY/2072.c
@@ -3,17 +3,17 @@
 int	main(void)
 {
 	int x;
+	int arr[10];
 
 	scanf("%d", &x);
-	int arr[10];
 
 	for (int i = 0; i < x; i++){
 		int sum = 0;
 		for (int j = 0; j < 10; j++){
-			scanf("%d ", arr[j]);
+			scanf("%d", &arr[j]);
 			if (arr[j] % 2 == 1)
 				sum += arr[j];
 		}
-		printf("#%d %d", x, sum);
+		printf("#%d %d\n", i + 1, sum);
 	}
 }
